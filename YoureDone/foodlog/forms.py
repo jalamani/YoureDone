559afd1b5b4ebc7d990b2fcdba011aa7296a40b6
jalamani <33540354+jalamani@users.noreply.github.com>
2019-06-08 14:58:03 -0700

    Delete forms.py

--- YoureDone/foodlog/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class newfoodForm(forms.Form):
-    newFood = forms.CharField(label='New food', max_length=100)
-    newCalories = forms.IntegerField(label="New food's calories", min_value=0, max_value=99999)
-class newlogForm(forms.Form):
-	newLog = forms.DateField(label='New date')#, max_length=100)
